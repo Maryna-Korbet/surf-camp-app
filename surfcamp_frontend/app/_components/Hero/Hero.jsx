a63b331c9b5224f66081cd3142b92e33ae8c95e3
Maryna-Korbet <m22v86k@gmail.com>
2024-10-30 23:44:40 -0400

    add Hero

--- surfcamp_frontend/app/_components/Hero/Hero.jsx
+++ surfcamp_frontend/app/_components/Hero/Hero.jsx
@@ -5,7 +5,8 @@ const Hero = ({imgSrc, headline, theme = 'turquoise'}) => {
     return (
         <section className='hero'>
             <div className="hero__background">
-                <img src={imgSrc || "/assets/home/hero-block/hero-home.jpg"} alt="Surfer on the waves" />
+                <img src={imgSrc || "/assets/home/hero-block/freepik__5.jpeg"} alt="Surfer on the waves" />
+                {/* <img src={imgSrc || "/assets/home/hero-block/hero-home.jpg"} alt="Surfer on the waves" /> */}
             </div>
             <div className={`hero__headline hero__headline--${theme}`}>
                 {headline}
